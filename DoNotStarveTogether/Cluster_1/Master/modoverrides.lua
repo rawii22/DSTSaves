79b84e6d19e8e2387a185daf0e836a3fdea9cd22
Alberto Romañach <albertoromanach@gmail.com>
2019-01-21 19:01:13 -0500

    Progress in Gentle Flutter

--- DoNotStarveTogether/Cluster_1/Master/modoverrides.lua
+++ DoNotStarveTogether/Cluster_1/Master/modoverrides.lua
@@ -1,6 +1,38 @@
 return {
+  ClientConsole={ configuration_options={  }, enabled=true },
+  PrefabIDConcatenator={ configuration_options={  }, enabled=true },
+  TorchCooking={ configuration_options={  }, enabled=true },
+  ["workshop-375859599"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
   ["workshop-382177939"]={
     configuration_options={ [""]=true, eightxten="5x16", workit="yep" },
+    enabled=true 
+  },
+  ["workshop-458940297"]={
+    configuration_options={
+      DFV_ClientPrediction="default",
+      DFV_FueledSettings="default",
+      DFV_Language="EN",
+      DFV_MinimalMode="default",
+      DFV_PercentReplace="default",
+      DFV_ShowACondition="default",
+      DFV_ShowADefence="default",
+      DFV_ShowAType="default",
+      DFV_ShowDamage="default",
+      DFV_ShowFireTime="default",
+      DFV_ShowInsulation="default",
+      DFV_ShowTemperature="default",
+      DFV_ShowUses="default" 
+    },
     enabled=true 
   } 
 }
